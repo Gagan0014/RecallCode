22b020b2139c364792fa0740707f030663c6ad52
Gagan0014 <gaganpathak2238@gmail.com>
2026-06-04 02:09:14 +0530

    added get routes for users

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -10,3 +10,15 @@ export const createUser = async(req,res)=>{
         });
     }
 }
+
+export const getUser = async(req,res) =>{
+    try{
+        const users = await User.find()
+
+        res.status(200).json(users);
+    }catch(error){
+        res.status(500).json({
+        message: error.message
+    });
+    }
+}
--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,8 +1,8 @@
 import express from 'express'
 
-import {createUser} from '../controllers/userController.js'
+import {getUser , createUser} from '../controllers/userController.js'
 const router = express.Router();
-router.get("/",createUser)
+router.get("/",getUser)
 router.post("/",createUser);
 router.put("/setting",createUser)
 
