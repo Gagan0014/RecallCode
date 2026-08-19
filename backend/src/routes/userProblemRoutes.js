2b05d5fa9eac925528b58136249f932e21c12167
Gagan Pathak <gaganpathak2238@gmail.com>
2026-08-20 00:37:56 +0530

    Add admin middleware to GET /api/userproblems route for security

--- backend/src/routes/userProblemRoutes.js
+++ backend/src/routes/userProblemRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import {protect} from '../middlewares/authMiddleware.js'
+import { admin } from '../middlewares/adminMiddleware.js'
 const router = express.Router()
 import { createUserProblem,getUserProblems } from '../controllers/userProblemController.js';
 router.post("/",protect,createUserProblem);
-router.get("/",protect,getUserProblems);
+router.get("/",protect,admin,getUserProblems);
 
 export default router;
