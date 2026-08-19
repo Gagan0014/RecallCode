de2faf47a0b302a0740b763939a69cc430db6a64
Gagan Pathak <gaganpathak2238@gmail.com>
2026-08-20 00:47:25 +0530

    Cleanup: Remove unused :username parameter from sync route (controller uses req.user.leetcodeUsername instead)

--- backend/src/routes/syncRoute.js
+++ backend/src/routes/syncRoute.js
@@ -4,5 +4,5 @@ const router = express.Router();
 import { syncProblems } from '../controllers/syncProblemController.js'
 import {protect} from '../middlewares/authMiddleware.js'
 
-router.post("/sync/:username",protect,syncProblems);
+router.post("/sync", protect, syncProblems);
 export default router
